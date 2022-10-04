edd788967641ad36e7f9f88c120665e63c2002bf
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-10-04 22:11:40 +0800

    rh hr rh rh

---
+++ components/ViewPayroll.vue
@@ -0,0 +1,21 @@
+<template>
+    <a-typography-title>Employee's Payrolls</a-typography-title>
+        <a-typography-text type="secondary">Here you can view payrolls, edit, download them</a-typography-text>
+        <a-table :columns="columns" :data-source="payrollData">
+            <template #bodyCell="{ column, record }">
+                <template v-if="column.key === 'action'">
+                    <span>
+                        <a @click="viewPayrollSingle(record)">View</a>
+                        <a-divider type="vertical" />
+                        <a @click="downloadPayroll(record)">Download</a>
+                        <a-divider type="vertical" />
+                        <a @click="downloadPayroll(record)">Edit</a>
+                    </span>
+                </template>
+            </template>
+        </a-table>
+</template>
+
+<script setup>
+
+</script>
