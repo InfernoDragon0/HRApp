40b47c38b54b4ba1df4bc6d93ceb9f8faca46592
InfernoDragon0 <mushroomapplepiele@gmail.com>
2022-10-05 12:35:51 +0800

    use .env file

--- server-middleware/index.ts
+++ server-middleware/index.ts
@@ -31,8 +31,8 @@ declare module 'express-session' { //DECLARE YOUR SEESSION VARIABLES HERE TO PAS
 //sql connection here
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: "root",
-    //password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
